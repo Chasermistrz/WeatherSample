5d85470d2cc79669f8deecf7660eb60798447972
Marek Liszewski <marek.liszewski@outlook.com>
2017-11-23 22:52:34 +0100

    Removed unecessary files

--- WeatherSample/AngularJS/Main.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="../Scripts/typings/angularjs/angular.d.ts" />
-var Main = /** @class */ (function () {
-    function Main() {
-    }
-    Main.WebApi = angular.module("WeatherApp.WebApi", []);
-    Main.App = angular.module("WeatherApp", ["WeatherApp.WebApi"]);
-    return Main;
-}());
-//# sourceMappingURL=Main.js.map
